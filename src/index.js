bef515528e8f6f0968de66c200302b24841b42a5
fish lin <spe9g0908@gmail.com>
2021-02-17 19:23:59 +0800

    login

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(session({
+    name:'login_mid',
     secret: 'sdkjghoif39097894508tyighdsgkgiso',
     saveUninitialized: false,
     resave: false,
@@ -55,43 +56,87 @@ app.get('/', (req, res)=>{
 app.post('/login', cors(corsOptions),async(req,res)=>{
  
     const [rows] = await db.query("SELECT * FROM member WHERE account=? AND password=?",[req.body.account, req.body.password])
-    if(rows.length===1){
+    if(rows.length===1)
+    {
         req.session.user = rows[0];
+        mid = rows[0].mid
+        const token = jwt.sign({mid}, process.env.JWT_KEY);
         res.json({
             success: true,
             body: rows[0],
-            
+            token,
         })
-    } else {
-        res.json({
-            success: false,
-            body: req.body
-        })
+    }   else {
+        const [account] = await db.query("SELECT * FROM member WHERE account=?" ,[req.body.account])
+        if(account.length===0){
+            res.json({
+                error : "帳號錯誤或不存在",
+                success: false,
+                body: req.body
+            })
+        }
+        const [password] = await db.query("SELECT * FROM member WHERE password=?" ,[req.body.password])
+        if(password.length===0){
+            res.json({
+                error : "密碼錯誤",
+                success: false,
+                body: req.body
+            })
+        }
+
     }
 })
 
-app.post('/login-jwt', async (req, res)=>{
-    const [rows] = await db.query("SELECT * FROM member WHERE account=? AND password=?",[req.body.account, req.body.password])
-const mid=rows[0].mid
-    if(rows.length===1){
-        const token = jwt.sign({mid}, process.env.JWT_KEY);
-        
+app.post('/loginverify', async (req, res)=>{
+    if(req.session.user.account===undefined)
+    {
         res.json({
+            login: false,
+        })
+    }
+    const [rows] = await db.query("SELECT * FROM member WHERE account=? AND password=?",[req.session.user.account, req.session.user.password])
+    if(rows.length===1)
+    {
+        res.json({
+            login: true,
+            body: rows[0],
+        })
+    }
+    
+})
+
+
+app.put('/edit', upload.none() , async(req,res) => {
+    const {username, tel, email, address, birthday} = req.body;
+    const data = {username, tel, email, address, birthday};
+
+    const [rows] = await db.query( "UPDATE `member` SET ? WHERE 1",[data])
+    // res.json({
+    //     success: rows.changedRows===1
+    // });
+    if(rows.changedRows===1){
+        res.json({
+            body: req.body,
             success: true,
-            body:rows[0],
-            token,
         })
     } else {
         res.json({
-            success: false,
-            body: req.body
-        })
+             error : "更新失敗",
+             success: false,
+             body: req.body
+         })
     }
+});
+
+
+
+app.post('/logout', (req, res)=>{
+    // req.session.destroy();
+    delete req.session.user;
+  res.json({
+        logout : true,
+    });
 })
-// app.get('/logout', (req, res)=>{
-//     delete req.session.admin;
-//     res.redirect('/');
-// })
 
 app.use((req, res)=>{
     res.type('text/plain');
