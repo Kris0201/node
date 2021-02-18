9f3f184fd5241763c9dbd959892df898525bcb11
akirahasten <70085834+akirahasten@users.noreply.github.com>
2021-02-18 10:47:50 +0800

    orders1

--- data/orders1.sql
+++ data/orders1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2021-02-18 09:51:14
+-- 產生時間： 2021-02-18 10:46:54
 -- 伺服器版本： 10.4.17-MariaDB
 -- PHP 版本： 7.3.25
 
@@ -32,7 +32,7 @@ CREATE TABLE `orders1` (
   `mid` int(11) NOT NULL,
   `amount` int(11) NOT NULL,
   `receiver` varchar(255) NOT NULL,
-  `receiver-mobile` varchar(255) NOT NULL,
+  `receiverMobile` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `order_date` datetime NOT NULL,
   `order_state` varchar(255) DEFAULT '訂單成立',
@@ -46,7 +46,7 @@ CREATE TABLE `orders1` (
 -- 傾印資料表的資料 `orders1`
 --
 
-INSERT INTO `orders1` (`sid`, `mid`, `amount`, `receiver`, `receiver-mobile`, `address`, `order_date`, `order_state`, `designated_date`, `designated_period`, `deliver_state`, `payment_type`) VALUES
+INSERT INTO `orders1` (`sid`, `mid`, `amount`, `receiver`, `receiverMobile`, `address`, `order_date`, `order_state`, `designated_date`, `designated_period`, `deliver_state`, `payment_type`) VALUES
 (67, 1, 7140, '', '', '', '2020-12-23 16:50:13', '訂單成立', '0000-00-00', '不指定', '未出貨', '貨到付款'),
 (68, 1, 5600, '', '', '', '2020-12-24 10:11:20', '訂單成立', '0000-00-00', '不指定', '未出貨', '貨到付款');
 
